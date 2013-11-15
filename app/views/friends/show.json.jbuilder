70f75d1ae59aad4dded2aef679211e09ab06036c
spaghetticode <andy@spaghetticode.it>
2013-11-15 11:25:33 +0100

    initial commit

---
+++ app/views/friends/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @friend, :name, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Example::Application.config.secret_key_base = 'ea53d1d3ddee59284a20086d73601968899d0ed79f5f0bb4ea3f29cacfaf121cff1011101171b914f5acabf876da4d78cbef09eabe20d23f8d894bddab1923a2'
